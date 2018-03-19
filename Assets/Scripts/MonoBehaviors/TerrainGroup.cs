d012009e90ac48d8ced3c5351830290f3cbb0839
Unknown <alvinquach91@gmail.com>
2018-03-18 22:33:12 -0700

    added basic LOD

--- Assets/Scripts/MonoBehaviors/TerrainGroup.cs
+++ Assets/Scripts/MonoBehaviors/TerrainGroup.cs
@@ -14,7 +14,13 @@ public class TerrainGroup : MonoBehaviour {
 
     [SerializeField] private float _heightScale = 1.0f;
 
-    [SerializeField] private int _downsample = 2;
+    [SerializeField] private int _baseDownsampleLevel = 2;
+
+    [SerializeField] private int _LODLevels = 2;
+
+    [SerializeField] private Material _material;
+
+    // TODO Add option to use linear LOD downsampling.
 
     // Use this for initialization
     void Start() {
@@ -23,11 +29,36 @@ public class TerrainGroup : MonoBehaviour {
             return;
         }
 
-        gameObject.AddComponent<MeshFilter>();
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        // Minimum base downsampling level should be 1.
+        _baseDownsampleLevel = _baseDownsampleLevel < 1 ? 1 : _baseDownsampleLevel;
 
-        // GenerateMesh(mesh, _scale, _heightScale);
-        DemToMeshUtils.GenerateMesh(_filePath, mesh, _surfaceGeometryType, _scale, _heightScale, _downsample);
+        // Add LOD group manager.
+        // TODO Make this a class member variable.
+        LODGroup lodGroup = gameObject.AddComponent<LODGroup>();
+        LOD[] lods = new LOD[_LODLevels + 1];
+
+        // Create a child GameObject containing a mesh for each LOD level.
+        for (int i = 0; i <= _LODLevels; i++) {
+            GameObject child = new GameObject();
+            child.transform.SetParent(transform);
+            child.name = "LOD_" + i;
+
+            // Add MeshRenderer to child, and to the LOD group.
+            MeshRenderer meshRenderer = child.AddComponent<MeshRenderer>();
+            lods[i] = new LOD(i == 0 ? 1 : Mathf.Pow(1 - (float)i / _LODLevels, 2), new Renderer[] { meshRenderer });
+
+            // Add material to the MeshRenderer.
+            if (_material != null) {
+                meshRenderer.material = _material;
+            }
+
+            Mesh mesh = child.AddComponent<MeshFilter>().mesh;
+            DemToMeshUtils.GenerateMesh(_filePath, mesh, _surfaceGeometryType, _scale, _heightScale, _baseDownsampleLevel * (int)Mathf.Pow(2, i));
+
+        }
+
+        lodGroup.SetLODs(lods);
+        lodGroup.RecalculateBounds();
 
     }
 
