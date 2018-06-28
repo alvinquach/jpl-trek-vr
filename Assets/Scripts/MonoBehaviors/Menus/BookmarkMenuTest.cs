874df9090f7023510b681e6504cc9a997d35bf97
Unknown <alvinquach91@gmail.com>
2018-06-27 23:00:31 -0700

    refactor bookmark menu code

--- Assets/Scripts/MonoBehaviors/Menus/BookmarkMenuTest.cs
+++ Assets/Scripts/MonoBehaviors/Menus/BookmarkMenuTest.cs
@@ -1,13 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.UI;
-using Newtonsoft.Json;
 
 public class BookmarkMenuTest : MonoBehaviour {
-
-    private static readonly string _bookmarksUrl = "https://marstrek.jpl.nasa.gov/TrekServices/ws/index/eq/searchItems?&&&&proj=urn:ogc:def:crs:EPSG::104905&start=0&rows=30&facetKeys=itemType&facetValues=bookmark&&resolutionMin=&resolutionMax=&noSort=false";
 
     private IList<Bookmark> _bookmarks;
 
@@ -21,7 +16,8 @@ public class BookmarkMenuTest : MonoBehaviour {
 
     void OnEnable() {
         if (_bookmarks == null) {
-            StartCoroutine(Test());
+            BookmarksWebService bookmarksWebService = WebServiceManager.Instance.BookmarksWebService;
+            bookmarksWebService.GetBookmarks(OnGetBookmarks);
         }
         ActivatePins(true);
     }
@@ -30,63 +26,49 @@ public class BookmarkMenuTest : MonoBehaviour {
         ActivatePins(false);
     }
 
-    IEnumerator Test() {
-        UnityWebRequest request = UnityWebRequest.Get(_bookmarksUrl);
-        yield return request.SendWebRequest();
+    private void OnGetBookmarks(IList<Bookmark> bookmarks) {
+        _bookmarks = bookmarks;
+        for (int i = 0; i < _bookmarks.Count; i++) {
+            Bookmark bookmark = _bookmarks[i];
+            Vector2 centerCoords = CalculateCenterCoordinates(bookmark.bbox);
 
-        if (request.isNetworkError || request.isHttpError) {
-            Debug.Log(request.error);
-        }
-        else {
+            if (buttonTemplate != null) {
+                GameObject obj = Instantiate(buttonTemplate, transform);
+                obj.transform.localPosition += 24 * i * Vector3.down;
 
-            string responseBody = request.downloadHandler.text;
+                XRMenuElement menuElem = obj.GetComponent<XRMenuElement>();
+                menuElem.latitude = centerCoords.x;
+                menuElem.longitude = centerCoords.y;
 
-            ResponseContainer<BookmarksResponse> response = JsonConvert.DeserializeObject<ResponseContainer<BookmarksResponse>>(responseBody);
+                Text text = obj.GetComponentInChildren<Text>();
+                text.text = bookmark.title;
 
-            _bookmarks = response.response.docs;
-
-            for (int i = 0; i < _bookmarks.Count; i++) {
-                Bookmark bookmark = _bookmarks[i];
-                Vector2 centerCoords = CalculateCenterCoordinates(bookmark.bbox);
-
-                if (buttonTemplate != null) {
-                    GameObject obj = Instantiate(buttonTemplate, transform);
-                    obj.transform.localPosition += 24 * i * Vector3.down;
-
-                    XRMenuElement menuElem = obj.GetComponent<XRMenuElement>();
-                    menuElem.latitude = centerCoords.x;
-                    menuElem.longitude = centerCoords.y;
-
-                    Text text = obj.GetComponentInChildren<Text>();
-                    text.text = bookmark.title;
-
-                    obj.SetActive(true);
-                }
-
-                if (pinTemplate != null && planet != null) {
-
-                    GameObject pin = Instantiate(pinTemplate, planet.transform);
-
-                    pin.transform.forward = planet.transform.forward;
-
-                    pin.transform.Rotate(planet.transform.right, -centerCoords.x, Space.World);
-                    pin.transform.Rotate(planet.transform.up, -centerCoords.y, Space.World);
-
-                    pin.transform.position = planet.transform.position + planet.transform.localScale.x * planet.scale * pin.transform.forward;
-                    
-                    pin.transform.forward = planet.transform.position - pin.transform.position;
-
-                    pin.transform.localScale = 4 * pin.transform.localScale;
-
-                    Text text = pin.GetComponentInChildren<Text>();
-                    text.text = bookmark.title;
-
-                    pins.Add(pin);
-                }
-
-                ActivatePins(true);
-
+                obj.SetActive(true);
             }
+
+            if (pinTemplate != null && planet != null) {
+
+                GameObject pin = Instantiate(pinTemplate, planet.transform);
+
+                pin.transform.forward = planet.transform.forward;
+
+                pin.transform.Rotate(planet.transform.right, -centerCoords.x, Space.World);
+                pin.transform.Rotate(planet.transform.up, -centerCoords.y, Space.World);
+
+                pin.transform.position = planet.transform.position + planet.transform.localScale.x * planet.scale * pin.transform.forward;
+
+                pin.transform.forward = planet.transform.position - pin.transform.position;
+
+                pin.transform.localScale = 4 * pin.transform.localScale;
+
+                Text text = pin.GetComponentInChildren<Text>();
+                text.text = bookmark.title;
+
+                pins.Add(pin);
+            }
+
+            ActivatePins(true);
+
         }
     }
 
