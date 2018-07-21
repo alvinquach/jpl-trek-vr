fe6aa383f8888644903a6d2c54cf051db48e696a
Unknown <alvinquach91@gmail.com>
2018-07-21 14:17:31 -0700

    created dem web services

---
+++ Assets/Scripts/MonoBehaviors/Services/Web/JPL/IDataElevationModelWebService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractDataElevationModelWebService : WebService {
+
+    public abstract void GetDEM(string resourceUrl, FileCallback callback);
+
+}
---
+++ Assets/Scripts/MonoBehaviors/Services/Web/JPL/MockDataElevationModelWebService.cs
@@ -0,0 +1,17 @@
+﻿
+/// <summary>
+///     For testing purposes only.
+///     Remove this implementation when it is no longer needed.
+/// </summary>
+public class MockDataElevationModelWebService : AbstractDataElevationModelWebService {
+
+    public override void ClearCache() {
+        // TODO Implement this
+    }
+
+    public override void GetDEM(string resourceUrl, FileCallback callback) {
+        // TODO Implement this
+    }
+
+}
+
