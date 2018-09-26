9970e21bdcff11516bd28812ea20869cad46f400
Unknown <alvinquach91@gmail.com>
2018-09-25 18:37:05 -0700

    moved TiffInfo into its own file

---
+++ Assets/Scripts/Models/Tiff/TiffInfo.cs
@@ -0,0 +1,28 @@
+﻿using BitMiracle.LibTiff.Classic;
+
+/// <summary>
+///     A data transfer object containing the information about a TIFF image.
+/// </summary>
+public class TiffInfo {
+
+    /// <summary> Width of the TIFF image in pixels. </summary>
+    public int Width;
+
+    /// <summary> height of the TIFF image in pixels. </summary>
+    public int Height;
+
+    /// <summary> Number of bits per pixel. </summary>
+    public short BPP;
+
+    /// <summary> Number of samples (color channels) per pixel. </summary>
+    public short SPP;
+
+    /// <summary> Sample data type (ie. float, int, etc.). </summary>
+    public string SampleFormat;
+
+    /// <summary> Compression method used by the TIFF image. </summary>
+    public Compression Compression;
+
+    /// <summary> Whether the TIFF is encoded with tiles rather than scanlines. </summary>
+    public bool Tiled;
+}
--- Assets/Scripts/Utils/TiffUtils.cs
+++ Assets/Scripts/Utils/TiffUtils.cs
@@ -62,28 +62,3 @@ public class TiffUtils {
     }
 
 }
-
-// TODO Move this to its own file.
-public class TiffInfo {
-
-    /// <summary> Width of the TIFF image in pixels. </summary>
-    public int Width;
-
-    /// <summary> height of the TIFF image in pixels. </summary>
-    public int Height;
-
-    /// <summary> Number of bits per pixel. </summary>
-    public short BPP;
-
-    /// <summary> Number of samples (color channels) per pixel. </summary>
-    public short SPP;
-
-    /// <summary> Sample data type (ie. float, int, etc.). </summary>
-    public string SampleFormat;
-
-    /// <summary> Compression method used by the TIFF image. </summary>
-    public Compression Compression;
-
-    /// <summary> Whether the TIFF is encoded with tiles rather than scanlines. </summary>
-    public bool Tiled;
-}
