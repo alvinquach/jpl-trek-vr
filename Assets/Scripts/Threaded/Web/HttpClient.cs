da54e103cc93468c4b0e06d5e512ba11b235dc59
Unknown <alvinquach91@gmail.com>
2018-11-15 22:59:44 -0800

    created class for threaded http requests (#7)

---
+++ Assets/Scripts/Threaded/Web/HttpClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+public class HttpClient {
+
+    public void Get(string uri, Action<HttpWebResponse> callback = null) {
+        Get(uri, null, callback);
+    }
+
+    public void Get(string uri, object options, Action<HttpWebResponse> callback = null) {
+
+        ThreadPool.QueueUserWorkItem((state) => {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse()) {
+                callback?.Invoke(response);
+            }
+
+        });
+
+    }
+
+}
