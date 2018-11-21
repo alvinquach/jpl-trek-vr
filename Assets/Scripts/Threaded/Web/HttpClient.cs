37b37cfcec2d383756bcdbb420e70cc3ebd2379c
Unknown <alvinquach91@gmail.com>
2018-11-21 13:35:44 -0800

    updated HttpClient methods
    - added support for POST requests
    - added error handling

--- Assets/Scripts/Threaded/Web/HttpClient.cs
+++ Assets/Scripts/Threaded/Web/HttpClient.cs
@@ -1,24 +1,93 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Net;
+using System.Text;
 using System.Threading;
+using UnityEngine;
 
 public class HttpClient {
 
-    public void Get(string uri, Action<HttpWebResponse> callback = null) {
-        Get(uri, null, callback);
-    }
-
-    public void Get(string uri, object options, Action<HttpWebResponse> callback = null) {
+    public void Get(string uri, Action<HttpWebResponse> callback = null, Action<HttpWebResponse> errorCallback = null) {
 
         ThreadPool.QueueUserWorkItem((state) => {
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse()) {
-                callback?.Invoke(response);
-            }
+            SendRequest(request, callback, errorCallback);
 
         });
 
+    }
+
+    public void Post(string uri, object body, Action<HttpWebResponse> callback = null, Action<HttpWebResponse> errorCallback = null) {
+
+        ThreadPool.QueueUserWorkItem((state) => {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Method = "POST";
+            SetContent(request, body);
+
+            SendRequest(request, callback, errorCallback);
+
+        });
+
+    }
+
+    private void SendRequest(HttpWebRequest request, Action<HttpWebResponse> callback, Action<HttpWebResponse> errorCallback) {
+
+        try {
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse()) {
+                callback?.Invoke(response);
+            }
+        }
+
+        catch (WebException e) {
+            using (HttpWebResponse response = (HttpWebResponse)e.Response) {
+                if (errorCallback != null) {
+                    errorCallback(response);
+                }
+                else {
+                    Debug.LogError(e.Message);
+                }
+            }
+        }
+
+    }
+
+    /// <summary>
+    ///     Helper method to set the request body for POST, PUT, and PATCH requests.
+    /// </summary>
+    /// <param name="request">The HttpWebRequest object.</param>
+    /// <param name="body">The request body data.</param>
+    private void SetContent(HttpWebRequest request, object body) {
+
+        // Convert body object into bytes
+        string contentString = ToJsonString(body);
+        byte[] contentBytes = Encoding.UTF8.GetBytes(contentString);
+
+        // Set content type and content length
+        request.ContentType = GetContentType(body);
+        request.ContentLength = contentBytes.Length;
+
+        // Wirte content to request stream
+        using (Stream dataStream = request.GetRequestStream()) {
+            dataStream.Write(contentBytes, 0, contentBytes.Length);
+        }
+
+    }
+
+    private static string ToJsonString(object data) {
+        if (data is string) {
+            return (string)data;
+        }
+        return JsonConvert.SerializeObject(data);
+    }
+
+    private static string GetContentType(object data) {
+        if (data is string) {
+            return "text/plain";
+        }
+        return "application/json";
     }
 
 }
