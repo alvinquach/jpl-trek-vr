2cd8c9101ffc62c103d9dc8c8e394f241bb5390a
Unknown <alvinquach91@gmail.com>
2018-11-27 12:46:53 -0800

    created a http request utility class

---
+++ Assets/Scripts/Http/Utils/HttpRequestUtils.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace TrekVRApplication.Http {
+
+    public static class HttpRequestUtils {
+
+        public static string AppendParams(string baseUrl, IDictionary<string, string> paramsMap, bool urlEncode = true) {
+            string paramsString = GenerateParamsString(paramsMap, urlEncode);
+            return AppendParams(baseUrl, paramsString);
+        }
+
+        public static string AppendParams(string baseUrl, string paramsString) {
+            return string.IsNullOrEmpty(paramsString) ? baseUrl : $"{baseUrl}?{paramsString}";
+        }
+
+        public static string GenerateParamsString(IDictionary<string, string> paramsMap, bool urlEncode = true) {
+            // TODO Implement this
+            return null;
+        }
+
+
+        public static string DecodeUrl(string url) {
+            return WebUtility.UrlDecode(url);
+        }
+
+        public static string EncodeUrl(string url) {
+            return WebUtility.UrlEncode(url);
+        }
+
+    }
+
+}
