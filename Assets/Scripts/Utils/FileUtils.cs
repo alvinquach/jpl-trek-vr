6345b9285b324d9b961bb4efd0f2748a3a43b13d
Unknown <alvinquach91@gmail.com>
2018-11-27 18:31:32 -0800

    added method for listing files in a directory

---
+++ Assets/Scripts/Utils/FileUtils.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TrekVRApplication {
+
+    public static class FileUtils {
+
+        public static IList<string> ListFiles(string directory, string searchPattern = "*", bool createIfNotExist = false) {
+            DirectoryInfo dirInfo = new DirectoryInfo(directory);
+            if (!dirInfo.Exists) {
+                if (createIfNotExist) {
+                    dirInfo.Create();
+                }
+                else {
+                    return new List<string>();
+                }
+            }
+            FileInfo[] files = dirInfo.GetFiles(searchPattern);
+            return files.Select(file => file.Name).ToList();
+        }
+
+    }
+
+}
